063c6de184111e371eb71712a4ac7d9458bf9a8a
lipe-oliveira <felipeoliveira.b@gmail.com>
2020-02-10 17:13:59 -0300

    nomral

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://deploy:homemaranha@cluster0-bgfbm.mongodb.net/test?retryWrites=true&w=majority');
+//mongoose.connect('mongodb+srv://deploy:homemaranha@cluster0-bgfbm.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://deploy:homemaranha@cluster0-bgfbm.mongodb.net/bibiboom?retryWrites=true&w=majority');
 
 mongoose.Promise = global.Promise;
 
