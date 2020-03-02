897e5c563664f0c799aefe983adeeb4aa1656f53
lipe-oliveira <felipeoliveira.b@gmail.com>
2020-03-01 21:03:30 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ app.use(cors());
 require('./app/controllers/authController')(app);
 require('./app/controllers/projectController')(app);
 
-app.listen(8081 || 3000);
+app.listen(process.env.PORT || 3000);
